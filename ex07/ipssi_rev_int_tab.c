4912977b6af83b0ce48d6beedd059c83ae8f0f08
KIfeon <theocanouel76@gmail.com>
2025-10-14 13:00:29 +0200

    ex08

--- ex07/ipssi_rev_int_tab.c
+++ ex07/ipssi_rev_int_tab.c
@@ -17,21 +17,3 @@ void	ipssi_rev_int_tab(int *tableau, int taille)
 		fin--;
 	}
 }
-
-int	main(void)
-{
-	int		tab[4];
-	char	c;
-
-	tab[4] = {1, 2, 3, 4};
-	ipssi_rev_int_tab(tab, 4);
-	c = tab[0] + '0';
-	write(1, &c, 1);
-	c = tab[1] + '0';
-	write(1, &c, 1);
-	c = tab[2] + '0';
-	write(1, &c, 1);
-	c = tab[3] + '0';
-	write(1, &c, 1);
-	return (0);
-}
---
+++ ex08/ipssi_sort_int_tab2.c
@@ -0,0 +1,20 @@
+void ipssi_sort_int_tab(int *tab, int taille)
+{
+    int dernier;
+    int j;
+
+    if (taille <= 1)
+        return;
+
+    ipssi_sort_int_tab(tab, taille - 1);
+
+    dernier = tab[taille - 1];
+    j = taille - 2;
+
+    while (j >= 0 && tab[j] > dernier)
+    {
+        tab[j + 1] = tab[j];
+        j--;
+    }
+    tab[j + 1] = dernier;
+}
