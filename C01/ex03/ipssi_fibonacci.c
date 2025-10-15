cdf76e54509e3177b49c32e692871c80cd0418f0
Your Name <you@example.com>
2025-10-15 15:50:56 +0200

    repare ex fibonacci

--- C01/ex03/ipssi_fibonacci.c
+++ C01/ex03/ipssi_fibonacci.c
@@ -1,25 +1,10 @@
 int	ipssi_fibonacci(int index)
 {
-	int	a;
-	int	b;
-	int	c;
-	int	deux;
-
 	if (index < 0)
 		return (-1);
 	if (index == 0)
 		return (0);
 	if (index == 1)
 		return (1);
-	a = 0;
-	b = 1;
-	deux = 2;
-	while (deux <= index)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		deux++;
-	}
-	return (b);
+	return (ipssi_fibonacci(index - 1) + ipssi_fibonacci(index - 2));
 }
