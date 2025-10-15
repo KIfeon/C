abc6f664174cbe1c47b3a75f636ba44d7c7eb7a7
Your Name <you@example.com>
2025-10-15 16:38:29 +0200

    C02 ex01

---
+++ C02/ex01/ipssi_strcat.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+char	*ipssi_strcat(char *dest, const char *src)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
